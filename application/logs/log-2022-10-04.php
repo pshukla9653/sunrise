dc4f26ee31d2ea6647291f4f3aafccf7f9858862
pshukla9653 <88384289+pshukla9653@users.noreply.github.com>
2022-10-13 11:11:08 +0530

    update

---
+++ application/logs/log-2022-10-04.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-10-04 07:50:28 --> Severity: Notice --> Only variables should be passed by reference D:\wamp64\www\sunrise\application\models\secure\Auth_model.php 37
+ERROR - 2022-10-04 07:50:28 --> Severity: Notice --> Only variables should be passed by reference D:\wamp64\www\sunrise\application\models\secure\Auth_model.php 43
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 16
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 16
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 16
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 16
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 26
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 16
+ERROR - 2022-10-04 07:50:30 --> Severity: Notice --> Undefined index: children D:\wamp64\www\sunrise\application\views\secure\template\side_menu.php 16
